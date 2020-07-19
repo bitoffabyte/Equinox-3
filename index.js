2ae0ed01d6b49cfdb089100bc8f94abd08b05066
R Narayan <nr.rnarayan@gmail.com>
2020-07-19 22:57:05 +0530

    faq

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const toggles = document.querySelectorAll('.faq-toggle');
+const faqItemHeaders = document.querySelectorAll(".faq-item-header");
 
-toggles.forEach(toggle => {
-toggle.addEventListener('click', () => {
-  toggle.parentNode.classList.toggle('active');
-});
+faqItemHeaders.forEach(faqItemHeader => {
+  faqItemHeader.addEventListener("click", event => {
+
+
+    const currentlyActivefaqItemHeader = document.querySelector(".faq-item-header.active");
+    if (currentlyActivefaqItemHeader && currentlyActivefaqItemHeader !== faqItemHeader) {
+      currentlyActivefaqItemHeader.classList.toggle("active");
+      currentlyActivefaqItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+
+    faqItemHeader.classList.toggle("active");
+    const faqItemBody = faqItemHeader.nextElementSibling;
+    if (faqItemHeader.classList.contains("active")) {
+      faqItemBody.style.maxHeight = faqItemBody.scrollHeight + "px";
+    }
+    else {
+      faqItemBody.style.maxHeight = 0;
+    }
+
+  });
 });
