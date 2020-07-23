08373309547204865d2c16557b4cbb0d07c7054a
Gauri Joshi <gjoshi.0522@gmail.com>
2020-07-23 16:48:03 +0530

    contact form

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-const faqItemHeaders = document.querySelectorAll(".faq-item-header");
+ // Your web app's Firebase configuration
+ var config = {
+  apiKey: "AIzaSyAf18cmX3bO3XunnumI-oBRbBLbCtX5raQ",
+  authDomain: "contactform-53f22.firebaseapp.com",
+  databaseURL: "https://contactform-53f22.firebaseio.com",
+  projectId: "contactform-53f22",
+  storageBucket: "contactform-53f22.appspot.com",
+  messagingSenderId: "158775277794",
+  appId: "1:158775277794:web:b66f6080947569e94d86bb",
+  measurementId: "G-KRZF5JXE0Y"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+
+var messagesRef = firebase.database().ref('messages');
+
+
+
+document.getElementById("ContactForm").addEventListener("submit", submitForm)
+ function submitForm(e)
+ {
+   e.preventDefault();
+
+   var email=getinputVal("email");
+   saveMessage(email);
+   document.getElementById('ContactForm').reset();
+ }
+ function getinputVal(id)
+ {
+   return document.getElementById(id).value;
+ }
+ function saveMessage(email)
+ {
+   var newMessageRef = messagesRef.push();
+   newMessageRef.set({
+     email:email
+   });
+ }
+
+
+
+ //faq
+ const faqItemHeaders = document.querySelectorAll(".faq-item-header");
 
 faqItemHeaders.forEach(faqItemHeader => {
   faqItemHeader.addEventListener("click", event => {
