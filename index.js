d2a78eb70316a6d6115400e88f571fd28236f8b3
R Narayan <nr.rnarayan@gmail.com>
2020-09-23 23:41:01 +0530

    button-fix

--- index.js
+++ index.js
@@ -57,27 +57,3 @@ faqItemHeaders.forEach((faqItemHeader) => {
         }
     });
 });
-
-// Initialize elements and events
-document.addEventListener('DOMContentLoaded', function () {
-    let devfolioOptions = {
-        buttonSelector: '#devfolio-apply-now',
-        // key: 'test', // put the hackathon key here
-    };
-
-    let script = document.createElement('script');
-    script.src = 'https://apply.devfolio.co';
-    document.head.append(script);
-
-    script.onload = function () {
-        new Devfolio(devfolioOptions);
-    };
-
-    script.onerror = function () {
-        document
-            .querySelector(devfolioOptions.buttonSelector)
-            .addEventListener('click', function () {
-                window.location.href = 'https://devfolio.co/signin';
-            });
-    };
-});
