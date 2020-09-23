73df172939669b96d00cb7f6ba1007be242ddfd6
R Narayan <nr.rnarayan@gmail.com>
2020-09-23 23:29:19 +0530

    button:

--- index.js
+++ index.js
@@ -1,66 +1,83 @@
- // Your web app's Firebase configuration
- var config = {
-  apiKey: "AIzaSyAf18cmX3bO3XunnumI-oBRbBLbCtX5raQ",
-  authDomain: "contactform-53f22.firebaseapp.com",
-  databaseURL: "https://contactform-53f22.firebaseio.com",
-  projectId: "contactform-53f22",
-  storageBucket: "contactform-53f22.appspot.com",
-  messagingSenderId: "158775277794",
-  appId: "1:158775277794:web:b66f6080947569e94d86bb",
-  measurementId: "G-KRZF5JXE0Y"
+// Your web app's Firebase configuration
+var config = {
+    apiKey: 'AIzaSyAf18cmX3bO3XunnumI-oBRbBLbCtX5raQ',
+    authDomain: 'contactform-53f22.firebaseapp.com',
+    databaseURL: 'https://contactform-53f22.firebaseio.com',
+    projectId: 'contactform-53f22',
+    storageBucket: 'contactform-53f22.appspot.com',
+    messagingSenderId: '158775277794',
+    appId: '1:158775277794:web:b66f6080947569e94d86bb',
+    measurementId: 'G-KRZF5JXE0Y',
 };
 // Initialize Firebase
 firebase.initializeApp(config);
 
-
 var messagesRef = firebase.database().ref('messages');
 
+document.getElementById('ContactForm').addEventListener('submit', submitForm);
+function submitForm(e) {
+    e.preventDefault();
 
+    var email = getinputVal('email');
+    saveMessage(email);
+    document.getElementById('ContactForm').reset();
+}
+function getinputVal(id) {
+    return document.getElementById(id).value;
+}
+function saveMessage(email) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        email: email,
+    });
+}
 
-document.getElementById("ContactForm").addEventListener("submit", submitForm)
- function submitForm(e)
- {
-   e.preventDefault();
+//faq
+const faqItemHeaders = document.querySelectorAll('.faq-item-header');
 
-   var email=getinputVal("email");
-   saveMessage(email);
-   document.getElementById('ContactForm').reset();
- }
- function getinputVal(id)
- {
-   return document.getElementById(id).value;
- }
- function saveMessage(email)
- {
-   var newMessageRef = messagesRef.push();
-   newMessageRef.set({
-     email:email
-   });
- }
+faqItemHeaders.forEach((faqItemHeader) => {
+    faqItemHeader.addEventListener('click', (event) => {
+        const currentlyActivefaqItemHeader = document.querySelector(
+            '.faq-item-header.active'
+        );
+        if (
+            currentlyActivefaqItemHeader &&
+            currentlyActivefaqItemHeader !== faqItemHeader
+        ) {
+            currentlyActivefaqItemHeader.classList.toggle('active');
+            currentlyActivefaqItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
 
+        faqItemHeader.classList.toggle('active');
+        const faqItemBody = faqItemHeader.nextElementSibling;
+        if (faqItemHeader.classList.contains('active')) {
+            faqItemBody.style.maxHeight = faqItemBody.scrollHeight + 'px';
+        } else {
+            faqItemBody.style.maxHeight = 0;
+        }
+    });
+});
 
+// Initialize elements and events
+document.addEventListener('DOMContentLoaded', function () {
+    let devfolioOptions = {
+        buttonSelector: '#devfolio-apply-now',
+        // key: 'test', // put the hackathon key here
+    };
 
- //faq
- const faqItemHeaders = document.querySelectorAll(".faq-item-header");
+    let script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co';
+    document.head.append(script);
 
-faqItemHeaders.forEach(faqItemHeader => {
-  faqItemHeader.addEventListener("click", event => {
+    script.onload = function () {
+        new Devfolio(devfolioOptions);
+    };
 
-
-    const currentlyActivefaqItemHeader = document.querySelector(".faq-item-header.active");
-    if (currentlyActivefaqItemHeader && currentlyActivefaqItemHeader !== faqItemHeader) {
-      currentlyActivefaqItemHeader.classList.toggle("active");
-      currentlyActivefaqItemHeader.nextElementSibling.style.maxHeight = 0;
-    }
-
-    faqItemHeader.classList.toggle("active");
-    const faqItemBody = faqItemHeader.nextElementSibling;
-    if (faqItemHeader.classList.contains("active")) {
-      faqItemBody.style.maxHeight = faqItemBody.scrollHeight + "px";
-    }
-    else {
-      faqItemBody.style.maxHeight = 0;
-    }
-
-  });
+    script.onerror = function () {
+        document
+            .querySelector(devfolioOptions.buttonSelector)
+            .addEventListener('click', function () {
+                window.location.href = 'https://devfolio.co/signin';
+            });
+    };
 });
