60bf732c49a2672a8467c37180cec47a17e7443a
R Narayan <nr.rnarayan@gmail.com>
2021-01-31 18:46:37 +0530

    wtf

--- index.js
+++ index.js
@@ -59,26 +59,26 @@ faqItemHeaders.forEach((faqItemHeader) => {
 });
 // devfolio registration button
 
-document.addEventListener('DOMContentLoaded', function () {
-	let devfolioOptions = {
-		buttonSelector: '#devfolio-apply-now',
-		key: 'equinox21',
-	};
+// document.addEventListener('DOMContentLoaded', function () {
+// 	let devfolioOptions = {
+// 		buttonSelector: '#devfolio-apply-now',
+// 		key: 'equinox21',
+// 	};
 
-	let script = document.createElement('script');
-	script.src = 'https://apply.devfolio.co';
-	document.head.append(script);
+// 	let script = document.createElement('script');
+// 	script.src = 'https://apply.devfolio.co';
+// 	document.head.append(script);
 
-	script.onload = function () {
-		new Devfolio(devfolioOptions);
-	};
+// 	script.onload = function () {
+// 		new Devfolio(devfolioOptions);
+// 	};
 
-	script.onerror = function () {
-		document
-			.querySelector(devfolioOptions.buttonSelector)
-			.addEventListener('click', function () {
-				window.location.href =
-					'https://devfolio.co/external-apply/' + devfolioOptions.key;
-			});
-	};
-});
+// 	script.onerror = function () {
+// 		document
+// 			.querySelector(devfolioOptions.buttonSelector)
+// 			.addEventListener('click', function () {
+// 				window.location.href =
+// 					'https://devfolio.co/external-apply/' + devfolioOptions.key;
+// 			});
+// 	};
+// });
